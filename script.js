ae4e62e1d216ce2d1019471c65ff181e1191e8c4
JoaquinFranzPayumo <156149491+JoaquinFranzPayumo@users.noreply.github.com>
2025-12-06 11:06:35 +0800

    Change theme toggle from 'dark' to 'blue'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
+    body.classList.toggle('blue');
 });
